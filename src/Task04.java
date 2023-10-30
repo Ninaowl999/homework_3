d541db10170b1968156d41aa8358a344b7a476d1
Ninaowl999 <128140719+Ninaowl999@users.noreply.github.com>
2023-10-30 21:08:02 +0400

    Commit add Task04.java

---
+++ src/Task04.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Task04 {
+    public static void main(String[] args) {
+        System.out.println("Задание №4");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите координату x: ");
+        double x = sc.nextDouble();
+        System.out.println("Введите координату y: ");
+        double y = sc.nextDouble();
+        if (x > 0 && y > 0) {
+            System.out.println("Точка находится в 1 четверти");
+        } else if (x < 0 && y > 0) {
+            System.out.println();
+        }
+        else if (x < 0 && y > 0) {
+            System.out.println("Точка находится в 2 четверти");
+        }
+        else if (x < 0 && y < 0) {
+            System.out.println("Точка находится в 3 четверти");
+        }
+        else if (x > 0 && y < 0) {
+            System.out.println("Точка находится в 4 четверти");
+        }
+        System.out.println();
+    }
+}
